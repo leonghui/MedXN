1f9fc02e242f79d625a26acae87cdce13da4c014
Leong Hui Wong <wong.leonghui@gmail.com>
2019-01-27 12:55:10 +0800

    License header cleanup

--- src/main/java/org/ohnlp/medxn/ae/ACLookupDrugNormAnnotator.java
+++ src/main/java/org/ohnlp/medxn/ae/ACLookupDrugNormAnnotator.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright: (c)  2013  Mayo Foundation for Medical Education and
  *  Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
  *  triple-shield Mayo logo are trademarks and service marks of MFMER.
@@ -20,7 +20,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *******************************************************************************/
+ */
 package org.ohnlp.medxn.ae;
 
 import java.io.IOException;
